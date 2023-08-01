28a404fd1f5f4f83757a244bdc5c1c09e4945108
Bryan Snyder <snyderb.de@gmail.com>
2023-07-31 21:02:30 -0400

    updates

--- code/game.py
+++ code/game.py
@@ -1,18 +1,23 @@
 from settings import *
 
 class Game:
+
     def __init__(self):
         
             # general 
             self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
             self.display_surface = pygame.display.get_surface()
             self.rect = self.surface.get_rect(topleft = (PADDING, PADDING))
+            self.sprites = pygame.sprite.Group()
 
             # lines 
             self.line_surface = self.surface.copy()
             self.line_surface.fill((0,255,0))
             self.line_surface.set_colorkey((0,255,0))
             self.line_surface.set_alpha(120)
+
+            # blocks
+            self.blocks = Block(self.sprites)
     
     # draw grid
     def draw_grid(self):
@@ -30,7 +35,17 @@ class Game:
         
             # drawing 
             self.surface.fill(GRAY)
+            self.sprites.draw(self.surface)
 
             self.draw_grid()
             self.display_surface.blit(self.surface, (PADDING,PADDING))
             pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, group, pos, color):
+        super().__init__(group)
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill(color)
+        
+        self.rect = self.image.get_rect(topleft = (0,0))
